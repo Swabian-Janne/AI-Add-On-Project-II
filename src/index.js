88548327626838a6b7b3781d66b0950bab9e2a0b
Swabian-Janne <janne.jasmin.jp@gmail.com>
2026-03-26 23:21:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 50,
+    delay: 1,
     cursor: "",
   });
 }
@@ -22,7 +22,7 @@ function generatePoem(event) {
   let apiKey = "ef3aab353f2ado9faa8tccd552b00647";
   let prompt = `User instructios: Generate a German poem about ${instructionsInput.value}.`; //Write a poem in the style of Goethe about the beauty of nature.
   let context =
-    "You are a poet who writes in the style of Goethe and other famous German poets of this time period. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions."; // giving the AI a "persona" to write in a specific style, so that the output is more accurate and relevant to the user's input
+    "You are a poet who writes in the style of Goethe and other famous German poets of this time period. Your mission is to generate a 4 line poem in basic HTML. Do not use a title. Make sure to follow the user instructions."; // giving the AI a "persona" to write in a specific style, so that the output is more accurate and relevant to the user's input
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=$(prompt)}&context=${context}&key=${apiKey}`;
 
   console.log("generating poem...");
