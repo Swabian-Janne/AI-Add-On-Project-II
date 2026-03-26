93dbb09abae023458c26cf59d361325e2549d2d5
Swabian-Janne <janne.jasmin.jp@gmail.com>
2026-03-26 21:01:01 +0100

    fixing typewriter deleting string problem

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@
 
 function generatePoem(event) {
   event.preventDefault();
-  // let poemElement = document.querySelector("#poem");
-  // poemElement.innerHTML = "Your German poem is being generated...";
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = "";
   //is not needed because of the typewriter effect, which will display the message as it is being generated.
 
   //import Typewriter from "typewriter-effect/dist/core";
 
-  new Typewriter("#poem", {
+new Typewriter(poemElement, {
     strings: ["Was bedächtlich Natur sonst unter viele verteilet"],
     autoStart: true,
-    delay: 1,
+    delay: 50
     cursor: "",
+    loop: false,
   });
-  // alert("Your German poem is being generated!");
+  
+
 }
 
 let poemFormElement = document.querySelector("#poem-form");
