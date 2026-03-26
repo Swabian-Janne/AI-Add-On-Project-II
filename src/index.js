1d442537b4f8546fe34d018c83dbb08ec13770ac
Swabian-Janne <janne.jasmin.jp@gmail.com>
2026-03-26 20:29:03 +0100

    Added basic form handing and typewriter Js PlugIn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 /** @format */
 
-/**alert(
-  "Welcome and Grüßgott, to the place where you can step into the literary realm of Goethe, Schiller and so many more!",
-);*/
+function generatePoem(event) {
+  event.preventDefault();
+  // let poemElement = document.querySelector("#poem");
+  // poemElement.innerHTML = "Your German poem is being generated...";
+  //is not needed because of the typewriter effect, which will display the message as it is being generated.
+
+  //import Typewriter from "typewriter-effect/dist/core";
+
+  new Typewriter("#poem", {
+    strings: ["Was bedächtlich Natur sonst unter viele verteilet"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+  // alert("Your German poem is being generated!");
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
